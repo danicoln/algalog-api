a30d3a03ec65e4746d029e9afa982bc84839c787
danicoln <lincolndeveloper@outlook.com>
2023-01-19 14:23:21 -0300

    Implementado o Bean Validation

--- src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -1,10 +1,12 @@
 package com.algaworks.algalog.domain.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -20,11 +22,16 @@ public class Cliente {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	@Column(name = "nome")
-	private String nome;
 	
+	@NotBlank
+	@Size(max = 60)
+	private String nome;
+	@NotBlank
+	@Email
+	@Size(max = 255)
 	private String email;
-	@Column(name = "telefone")
+	@Size(max = 20)
+	@NotBlank
 	private String telefone;
 	
 	
