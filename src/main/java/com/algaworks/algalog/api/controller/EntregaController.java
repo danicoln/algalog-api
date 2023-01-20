d8d7cf313445988b95022bec8dd36c3207bcdaac
danicoln <lincolndeveloper@outlook.com>
2023-01-20 13:38:19 -0300

    Exemplo de isolar o Domain Model do Representation Model

--- src/main/java/com/algaworks/algalog/api/controller/EntregaController.java
+++ src/main/java/com/algaworks/algalog/api/controller/EntregaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algalog.api.dto.DestinatarioDto;
+import com.algaworks.algalog.api.dto.EntregaDto;
 import com.algaworks.algalog.domain.model.Entrega;
 import com.algaworks.algalog.domain.repository.EntregaRepository;
 import com.algaworks.algalog.domain.service.SolicitacaoEntregaService;
@@ -27,6 +30,7 @@ public class EntregaController {
 
 	private EntregaRepository entregaRepository;
 	private SolicitacaoEntregaService solicitacaoEntregaService;
+	private ModelMapper modelMapper;
 	
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
@@ -40,10 +44,28 @@ public class EntregaController {
 	}
 	
 	@GetMapping("/{id}")
-	public ResponseEntity<Entrega> buscar(@PathVariable Long id){
+	public ResponseEntity<EntregaDto> buscar(@PathVariable Long id){
 		return entregaRepository.findById(id)
-				.map(ResponseEntity::ok)
-				.orElse(ResponseEntity.notFound().build());
+				.map(entrega -> {
+					EntregaDto entregaDto = new EntregaDto();
+					entregaDto.setId(entrega.getId());
+					entregaDto.setNomeCliente(entrega.getCliente().getNome());
+					
+					entregaDto.setDestinatario(new DestinatarioDto());
+					entregaDto.getDestinatario().setNome(entrega.getDestinatario().getNome());
+					entregaDto.getDestinatario().setLogradouro(entrega.getDestinatario().getLogradouro());
+					entregaDto.getDestinatario().setNumero(entrega.getDestinatario().getNumero());
+					entregaDto.getDestinatario().setComplemento(entrega.getDestinatario().getComplemento());
+					entregaDto.getDestinatario().setBairro(entrega.getDestinatario().getBairro());
+					
+					entregaDto.setTaxa(entrega.getTaxa());
+					entregaDto.setStatus(entrega.getStatus());
+					entregaDto.setDataPedido(entrega.getDataPedido());
+					entregaDto.setDataFinalizacao(entrega.getDataFinalizacao());
+					
+					return ResponseEntity.ok(entregaDto);
+				}).orElse(ResponseEntity.notFound().build());
+				
 	}
 	
 }
